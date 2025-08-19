d623092bf698884b1313b23841335ef880c60fb0
S. MohammadMahdi Zamanian <dev@mm25zamanian.ir>
2025-08-20 03:22:34 +0330

    refactor(apps/demo): replace Server with PrismateAdmin for improved functionality

--- apps/demo/lib/prismate.ts
+++ apps/demo/lib/prismate.ts
@@ -1,6 +1,5 @@
-import { Server } from "prismate";
+import { PrismateAdmin } from "prismate";
 import { db } from "./prisma"; // Prisma Client instance
 import { Prisma } from "@/generated/prisma";
 
-const x = new Server(db, Prisma.dmmf);
-export const { orpc, router } = x.createOrpc();
+export const x = new PrismateAdmin(db as any, Prisma.dmmf);
